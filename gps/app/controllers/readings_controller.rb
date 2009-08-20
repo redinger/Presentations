e806d3dd0f524f807bb12475d61be55ad513115f
Christopher Redinger <redinger@gmail.com>
2009-08-20 11:26:27 -0400

    reverse order readings

--- gps/app/controllers/readings_controller.rb
+++ gps/app/controllers/readings_controller.rb
@@ -1,5 +1,5 @@
 class ReadingsController < ApplicationController
   def index
-    @readings = Reading.paginate(:page => params[:page])
+    @readings = Reading.paginate(:page => params[:page], :order => ['updated_at desc'])
   end
 end
